bb51337d8d5840f8794c998348b39c2452cc383b
Prathamesh Anwekar <f20201039@pilani.bits-pilani.ac.in>
2023-02-08 13:43:25 +0530

    added hive

--- lib/storage/sharedPrefs.dart
+++ lib/storage/sharedPrefs.dart
@@ -1,32 +1,32 @@
-import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
 
-class SharedPrefs {
-  static SharedPreferences? prefs;
+// class SharedPrefs {
+//   static SharedPreferences? prefs;
 
-  static Future init() async => prefs = await SharedPreferences.getInstance();
+//   static Future init() async => prefs = await SharedPreferences.getInstance();
 
-  static  setBool(String key, bool value) async {
-    await prefs!.setBool(key, value);
-  }
+//   static  setBool(String key, bool value) async {
+//     await prefs!.setBool(key, value);
+//   }
 
-  static  setString(String key, String value) async {
-    await prefs!.setString(key, value);
-  }
+//   static  setString(String key, String value) async {
+//     await prefs!.setString(key, value);
+//   }
 
- static getBool(String key) {
-    return prefs!.getBool(key);
-  }
+//  static getBool(String key) {
+//     return prefs!.getBool(key);
+//   }
 
-  static  getString(String key) {
-    return prefs!.getString(key);
-  }
+//   static  getString(String key) {
+//     return prefs!.getString(key);
+//   }
 
-  static  setInt(String key, int value) async {
-    await prefs!.setInt(key, value);
-  }
+//   static  setInt(String key, int value) async {
+//     await prefs!.setInt(key, value);
+//   }
 
-  static getInt(String key) {
-    return prefs!.getInt(key) ;
-  }
+//   static getInt(String key) {
+//     return prefs!.getInt(key) ;
+//   }
   
-}
+// }
