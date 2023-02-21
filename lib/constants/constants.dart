89fda3dd910253a4393532268c263c76cc901173
Prathamesh Anwekar <f20201039@pilani.bits-pilani.ac.in>
2023-02-21 22:25:39 +0530

    internship backend done

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 class Constants {
-  static final statusList = [
-    ["Accepted", Colors.green],
-    ["Pending", Colors.yellow.shade700],
-    ["Rejected", Colors.red],
-  ];
+  static  Map<String, dynamic>  statusList = {
+    "Accepted": Colors.green,
+    "Pending": Colors.yellow.shade700,
+    "Rejected": Colors.red,
+};
 
   static final bottomBarIcons = [
     "assets/Icons/home.svg",
