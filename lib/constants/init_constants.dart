c882e2efe96e743db8191dfc8237cbe9ba3d3825
Prathamesh Anwekar <f20201039@pilani.bits-pilani.ac.in>
2023-02-25 12:08:07 +0530

    minor change

--- lib/constants/init_constants.dart
+++ lib/constants/init_constants.dart
@@ -1,10 +1,14 @@
+import 'dart:io';
+
 import 'package:bitsapp/firebase_options.dart';
 import 'package:bitsapp/services/fcm_service.dart';
 import 'package:bitsapp/services/notif_service.dart';
 import 'package:bitsapp/storage/hiveStore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_displaymode/flutter_displaymode.dart';
 
 class InitConstants {
   init() async {
@@ -15,8 +19,15 @@ class InitConstants {
     await NotifService.init().then((value) {
       NotifService.dismissAllNotifs();
     });
+    //set notif panel to transpaernt
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+    ));
     await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-    // if (!kIsWeb) await FlutterDisplayMode.setHighRefreshRate();
+  
+
+    if (Platform.isAndroid) await FlutterDisplayMode.setHighRefreshRate();
 
     await HiveStore.init();
   }
