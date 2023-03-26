4c6a01293c8553a5cc1fc02e6efdcf62a5ea3c93
Prathamesh Anwekar <f20201039@pilani.bits-pilani.ac.in>
2023-03-26 18:38:18 +0530

    conflicts

--- lib/views/Feed_Screen/Feed_Container/feed_container.dart
+++ lib/views/Feed_Screen/Feed_Container/feed_container.dart
@@ -10,7 +10,6 @@ class FeedContainer extends StatelessWidget {
   final FeedPost feedPost;
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     return SizedBox(
   
       child: GestureDetector(
@@ -25,38 +24,16 @@ class FeedContainer extends StatelessWidget {
             borderRadius: const BorderRadius.all(Radius.circular(8)),
           ),
           child: FeedDesc(
-=======
-    return Container(
-      padding: const EdgeInsets.all(5),
-      decoration: BoxDecoration(
-        color: Colors.white.withOpacity(1),
-        borderRadius: const BorderRadius.all(Radius.circular(8)),
-      ),
-      child: Column(
-        children: [
-          FeedDesc(
->>>>>>> 6a4dc44cbcd7da7c3f51d6bdc45c3d3ac5472fe7
             isFeedScreen: true,
             feedPost: feedPost,
             commentFunc: () => Navigator.of(context).push(
               MaterialPageRoute(
-<<<<<<< HEAD
                 builder: (context) =>
                      FeedDetailScreen(isCommentPressed: true, feedPost: feedPost,),
               ),
             ),
           ),
         ),
-=======
-                builder: (context) => FeedDetailScreen(
-                  isCommentPressed: true,
-                  feedPost: feedPost,
-                ),
-              ),
-            ),
-          ),
-        ],
->>>>>>> 6a4dc44cbcd7da7c3f51d6bdc45c3d3ac5472fe7
       ),
     );
   }
