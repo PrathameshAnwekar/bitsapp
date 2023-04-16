ed42c93121091a8104eecec54edb16f5e70eac7e
Arya <83908779+aryad-16@users.noreply.github.com>
2023-04-16 15:17:18 +0530

    mc

--- lib/views/Components/person_detail.dart
+++ lib/views/Components/person_detail.dart
@@ -27,7 +27,9 @@ class PersonDetail extends StatelessWidget {
           ),
         ),
         Text(
-         time != null ?  timeAgo(DateTime.fromMillisecondsSinceEpoch(time!)) : "",
+          time != null
+              ? timeAgo(DateTime.fromMillisecondsSinceEpoch(time!))
+              : "",
           style: GoogleFonts.firaSans(
             color: const Color.fromARGB(255, 166, 166, 166),
             letterSpacing: 0.2,
@@ -40,18 +42,24 @@ class PersonDetail extends StatelessWidget {
 }
 
 String timeAgo(DateTime d) {
- Duration diff = DateTime.now().difference(d);
- if (diff.inDays > 365)
-  return "${(diff.inDays / 365).floor()} ${(diff.inDays / 365).floor() == 1 ? "year" : "years"} ago";
- if (diff.inDays > 30)
-  return "${(diff.inDays / 30).floor()} ${(diff.inDays / 30).floor() == 1 ? "month" : "months"} ago";
- if (diff.inDays > 7)
-  return "${(diff.inDays / 7).floor()} ${(diff.inDays / 7).floor() == 1 ? "week" : "weeks"} ago";
- if (diff.inDays > 0)
-  return "${diff.inDays} ${diff.inDays == 1 ? "day" : "days"} ago";
- if (diff.inHours > 0)
-  return "${diff.inHours} ${diff.inHours == 1 ? "hour" : "hours"} ago";
- if (diff.inMinutes > 0)
-  return "${diff.inMinutes} ${diff.inMinutes == 1 ? "minute" : "minutes"} ago";
- return "just now";
+  Duration diff = DateTime.now().difference(d);
+  if (diff.inDays > 365) {
+    return "${(diff.inDays / 365).floor()} ${(diff.inDays / 365).floor() == 1 ? "year" : "years"} ago";
+  }
+  if (diff.inDays > 30) {
+    return "${(diff.inDays / 30).floor()} ${(diff.inDays / 30).floor() == 1 ? "month" : "months"} ago";
+  }
+  if (diff.inDays > 7) {
+    return "${(diff.inDays / 7).floor()} ${(diff.inDays / 7).floor() == 1 ? "week" : "weeks"} ago";
+  }
+  if (diff.inDays > 0) {
+    return "${diff.inDays} ${diff.inDays == 1 ? "day" : "days"} ago";
+  }
+  if (diff.inHours > 0) {
+    return "${diff.inHours} ${diff.inHours == 1 ? "hour" : "hours"} ago";
+  }
+  if (diff.inMinutes > 0) {
+    return "${diff.inMinutes} ${diff.inMinutes == 1 ? "minute" : "minutes"} ago";
+  }
+  return "just now";
 }
